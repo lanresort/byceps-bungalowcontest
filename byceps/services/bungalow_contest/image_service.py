fe39a7fb3dd5cc18a77611ea07693f51fdd21bd5
Jochen Kupperschmidt <homework@nwsnet.de>
2022-10-09 00:24:34 +0200

    Update copyright notes for 2022

--- byceps/services/bungalow_contest/image_service.py
+++ byceps/services/bungalow_contest/image_service.py
@@ -2,7 +2,7 @@
 byceps.services.bungalow_contest.image_service
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-:Copyright: 2006-2021 Jochen Kupperschmidt
+:Copyright: 2006-2022 Jochen Kupperschmidt
 :License: Revised BSD (see `LICENSE` file for details)
 """
 
