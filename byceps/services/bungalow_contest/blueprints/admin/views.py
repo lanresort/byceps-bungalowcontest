373f2ed4aa35fd8fcf0cd5a9acd2e618e03f755e
Jochen Kupperschmidt <homework@nwsnet.de>
2025-04-02 01:14:56 +0200

    Move admin blueprint into corresponding service package

--- byceps/services/bungalow_contest/blueprints/admin/views.py
+++ byceps/services/bungalow_contest/blueprints/admin/views.py
@@ -1,6 +1,6 @@
 """
-byceps.blueprints.admin.bungalow_contest.views
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+byceps.services.bungalow_contest.blueprints.admin.views
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 :Copyright: 2014-2025 Jochen Kupperschmidt
 :License: Revised BSD (see `LICENSE` file for details)
