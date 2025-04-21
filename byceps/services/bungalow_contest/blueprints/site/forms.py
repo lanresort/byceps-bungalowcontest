729a29662d32c3bc8f1879ee4451b4c60f686707
Jochen Kupperschmidt <homework@nwsnet.de>
2025-04-21 13:42:59 +0200

    Move site site blueprint into corresponding service package

--- byceps/services/bungalow_contest/blueprints/site/forms.py
+++ byceps/services/bungalow_contest/blueprints/site/forms.py
@@ -1,6 +1,6 @@
 """
-byceps.blueprints.site.bungalow_contest.forms
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+byceps.services.bungalow_contest.blueprints.site.forms
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 :Copyright: 2014-2025 Jochen Kupperschmidt
 :License: Revised BSD (see `LICENSE` file for details)
--- byceps/services/bungalow_contest/blueprints/site/views.py
+++ byceps/services/bungalow_contest/blueprints/site/views.py
@@ -1,6 +1,6 @@
 """
-byceps.blueprints.site.bungalow_contest.views
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+byceps.services.bungalow_contest.blueprints.site.views
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 :Copyright: 2014-2025 Jochen Kupperschmidt
 :License: Revised BSD (see `LICENSE` file for details)
